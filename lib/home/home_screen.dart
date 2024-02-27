9a8b5bcca0fa6f3bca8ab91d577e84427bf20f24
Your Name <you@example.com>
2024-02-27 21:31:08 +0200

    new pages

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -10,6 +10,17 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: const [
+          Center(
+            child: Text(
+              "Merhaba",
+              style: TextStyle(fontSize: 30),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
